e5b5b1ae993844f30db319e02be5b7f55863e22d
Oliver Lieshoff <o.lieshoff@schuetze-consulting.ag>
2022-09-15 11:13:43 +0200

    Added a "ParameterizedTest".

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -35,3 +35,15 @@ func TestFizzBuzz15(t *testing.T) {
 		t.Errorf("expected <%q> but was <%q>", expected, returned)
 	}
 }
+
+func TestFizzBuzzBatch(t *testing.T) {
+	expected := []string{"1", "fizz", "buzz", "7", "fizzbuzz"}
+	passed := []int{1, 3, 5, 7, 15}
+	for i := 0; i < len(passed); i++ {
+		returned := FizzBuzz(passed[i])
+		if expected[i] != returned {
+			t.Errorf("test position %q: expected <%q> but was <%q>", i, expected[i], returned)
+		}
+		t.Logf("passed step %d", i)
+	}
+}
