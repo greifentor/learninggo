e9667b3eca24607e33b7488e48f8853af1b6caa0
Oliver Lieshoff <o.lieshoff@schuetze-consulting.ag>
2022-09-22 08:37:26 +0200

    Corrections.

--- goproxyreader.go
+++ goproxyreader.go
@@ -29,17 +29,11 @@ func ReadLatest(proxy string, path string) (*LatestInfo, error) {
 
 /*
 
-    The client should at least support the info and @latest endpoints. list might be a good addition, but is not strictly required.
-    mod and zip endpoints are not used in this project but can serve as an additional exercise.
-    The client should support proxies running at different locations with https://proxy.golang.org/ being the default.
-    All operations should receive a context.Context as the first parameter and respect any deadline applied to the context.
-    Try to come up with an idiomatic API.
-    Write a unit test.
+   The client should at least support the info and @latest endpoints. list might be a good addition, but is not strictly required.
+   mod and zip endpoints are not used in this project but can serve as an additional exercise.
+   The client should support proxies running at different locations with https://proxy.golang.org/ being the default.
+   All operations should receive a context.Context as the first parameter and respect any deadline applied to the context.
+   Try to come up with an idiomatic API.
+   Write a unit test.
 
 */
-
-func ReadInfo() {
-
-}
-
-func ()
